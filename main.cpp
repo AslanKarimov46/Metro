97f242d5de8a227ad7870a66146ba6e5305791a7
Bloxin <maxretro4rever@gmail.com>
2024-12-14 13:12:53 +0400

    add GreenLine and metro emulation

--- main.cpp
+++ main.cpp
@@ -1,9 +1,39 @@
 #include <iostream>
-
+#include <vector>
+#include <string>
+#include <algorithm>
 
 int main(){
-	
-	std::cout<<"Hello\n";
-       std::cout << "It's me, Carl. Chill, chill. " << '\n';
-return 0;	
+	std::vector<std::string> GreenLine {
+		"Сырец",
+		"Дорогожичи",
+		"Лукьяновская",
+		"Золотые Ворота",
+		"Дворец Спорта",
+		"Кловская",
+		"Печерская",
+		"Дружбы Народов",
+		"Выдубичи",
+		"Славутич",
+		"Осокорки",
+		"Позняки",
+		"Харьковская",
+		"Вырлица",
+		"Бориспольская",
+		"Красный Хутор"
+	};
+	std::string Start, End;
+	std::getline(std::cin, Start);
+	std::getline(std::cin, End);
+	auto StartPos = std::find(GreenLine.begin(), GreenLine.end(), Start);
+	auto EndPos = std::find(GreenLine.begin(), GreenLine.end(), End);
+	while(StartPos < EndPos) {
+		StartPos++;
+		std::cout << "Qaplar bağlanır. Növbəti stansiyası - " << GreenLine[StartPos-GreenLine.begin()] << '\n';
+	}
+	while(StartPos > EndPos) {
+		StartPos--;
+		std::cout << "Qaplar bağlanır. Növbəti stansiyası - " << GreenLine[StartPos-GreenLine.begin()] << '\n';
+	}
+	return 0;
 }
