f6cbd2293addb7601f88959f2374e7ee650ed706
Bloxin <maxretro4rever@gmail.com>
2024-12-21 18:48:21 +0400

    Fully reorganized main.cpp, it's not too shame to watch

--- main.cpp
+++ main.cpp
@@ -6,9 +6,10 @@ int main(){
 	std::getline(std::cin, destinationStation);
 	std::pair<std::string, int> Coord1, Coord2;
 	find_stations(startStation, destinationStation, Coord1, Coord2);
-	std::cout<<"Номер 1ой станции: "<<Coord1.second<<"\nЦвет линии первой станции: "<<Coord1.first<<"\n";
-	std::cout<<"Номер 2ой станции: "<<Coord2.second<<"\nЦвет линии второй станции: "<<Coord2.first<<"\n";
-	std::cout<<route(startStation, destinationStation)<<"\n";
-
+    std::cout << "Линия начальной станции: " << Coord1.first << '\n';
+    std::cout << "Номер начальной станции:  " << Coord1.second+1 << '\n';   //То что в проге индексы с 0 считают, не значит что люди так считают
+    std::cout << "Линия станции назначения: " << Coord2.first << '\n';
+    std::cout << "Номер станции назначения:  " << Coord2.second+1 << '\n';   //То что в проге индексы с 0 считают, не значит что люди так считают
+	std::cout << "Время кратчайшего маршрута: " << route(startStation, destinationStation)<<"\n";
 	return 0;
 }
